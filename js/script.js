0155eb20ad3a6992cc51512f5f78769c9a40f925
fredeskrubba <frederikskrubbeltrang@outlook.dk>
2022-07-08 15:00:04 +0200

    added delete functionality

--- js/script.js
+++ js/script.js
@@ -2,7 +2,18 @@ const taskInput = document.querySelector("#input-field");
 const taskAddButton = document.querySelector("#add-task-button");
 const container = document.querySelector("#list");
 
+const taskArray = [];
 
+function updateTasks(){
+    container.innerHTML = "";
+    taskArray.forEach(task => {
+        container.appendChild(task)
+    });
+}
+
+function removeTask(){
+
+}
 function addTask(text){
     const newTask = document.createElement("div");
     const taskGrid = document.createElement("div");
@@ -22,14 +33,20 @@ function addTask(text){
     newTask.classList.add("bg-gray-100", "w-1/2", "p-6", "text-lg", "rounded-lg", "my-5");
     taskGrid.classList.add("container", "flex", "grid-cols-3", "gap-5");
     
-    container.appendChild(newTask);
     newTask.appendChild(taskGrid);
     taskGrid.appendChild(taskText);
     taskGrid.appendChild(taskEdit);
     taskGrid.appendChild(taskRemove);
+    taskArray.push(newTask);
+    updateTasks();
+    taskRemove.addEventListener("click", (e)=>{
+        taskArray.splice(taskArray.indexOf(e.target.parentElement.parentElement), 1);
+        updateTasks();
+    })
 }
 
 taskAddButton.addEventListener("click", ()=>{
     addTask(taskInput.value);
     taskInput.value = "";
 })
+
